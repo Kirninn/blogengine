dafdba23219835e129769fd561fabd17da02d780
Denis <44545518+Kirninn@users.noreply.github.com>
2020-05-06 14:14:10 +0500

    Правки девятого урока.

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('', post_list, name='url_blog_list'),
     path('post/create/', PostCreate.as_view(), name = 'url_post_create'),
     path('post/<str:slug>/', PostDetail.as_view(), name='url_post_detail'),
+    path('post/<str:slug>/update/', PostUpdate.as_view(), name = 'url_post_update'),
     path('tags/', tags_list, name='url_tags_list'),
     path('tags/create/', TagCreate.as_view(), name='url_tag_create'),
     path('tags/<str:slug>/', TagDetail.as_view(), name='url_tag_detail'),
+    path('tags/<str:slug>/update/', TagUpdate.as_view(), name = 'url_tag_update'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,14 +9,14 @@ def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
 
-
-def tags_list(request):
-    tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -27,7 +27,18 @@ class TagCreate(ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
 
-
 class PostCreate(ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create.html'
+
+
+class TagUpdate(ObjectUpdateMixin,View):
+    model = Tag
+    form_model = TagForm
+    template = 'blog/tag_update_form.html'
+
+
+class PostUpdate(ObjectUpdateMixin,View):
+    model = Post
+    form_model = PostForm
+    template = 'blog/post_update_form.html'
